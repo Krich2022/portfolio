3cc355e4976defda8400780442f2a0240325ea83
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-01-24 21:28:45 -0700

    created nav bar and debugging

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,24 @@
-const Nav = () => {
-  <nav>
-    <a href="#home">Home</a>
-    <a href="#about-me">About</a>
-    <a href="#projects">Projects</a>
-    <a href="#contact">Contact</a>
-  </nav>;
-};
+import Navbar from "./UI/Navbar";
+import { Link } from "react-router-dom";
 
-module.exports = Nav;
+export default function Nav() {
+  <Navbar
+    links={[
+      <Link key={1} to="/">
+        Home
+      </Link>,
+      <Link key={2} to="portfolio">
+        Portfolio
+      </Link>,
+      <Link key={3} to="about">
+        About Me
+      </Link>,
+      <Link key={4} to="contact">
+        Contact Me
+      </Link>,
+      <Link key={5} to="resume">
+        Resume
+      </Link>,
+    ]}
+  />;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from "./App";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ResumePage from "./pages/ResumePage";
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "resume",
+        element: <ResumePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "portfolio",
+        element: <PortfolioPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
